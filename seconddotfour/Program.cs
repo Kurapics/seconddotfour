97f6cabcaf523d090155711a1a81b53f962b5bd2
Компьютер <@DESKTOP-0RODP2R>
2023-04-25 22:31:22 +0200

    Добавьте файлы проекта.

---
+++ seconddotfour/Program.cs
@@ -0,0 +1,26 @@
+﻿Random r  = new Random();
+
+int[,] mas = new int[15, 15];
+
+for (int i = 0; i < 15; i++)
+{
+    for(int j = 0; j < 15; j++)
+    {
+        mas[i, j] = r.Next(0, 20);
+    }
+}
+
+for (int i = 0; i < 15; i++)
+{
+    for (int j = 0;j < 15; j++)
+    {
+        if (mas[i, 0] == 1)
+        {
+            Console.WriteLine("Строка номер: " + i);
+            int allNumsInRow = 0;
+            for (int f = 0; f < 15; f++)
+                allNumsInRow += mas[i, f];
+            Console.WriteLine("Среднее арифметическое элементов строки: " + (allNumsInRow / 15));
+        }
+    }
+}
